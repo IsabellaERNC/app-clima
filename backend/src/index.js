c938c20766b791eb236815d5ffb9d8f07e41e1ba
IsabellaERNC <isabellaescobarhernandez@gmail.com>
2026-03-24 23:53:12 -0500

    backend conectado a MongoDB

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend App de Clima funcionando ✅" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
